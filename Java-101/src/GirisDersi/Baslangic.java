b3d0511f9dbd62cc9541688938af54f53c4353b1
Aslı Temel <94898140+aslttml@users.noreply.github.com>
2021-12-28 18:40:28 +0300

    Delete Java-101 directory

--- Java-101/src/GirisDersi/Baslangic.java
+++
@@ -1,9 +0,0 @@
-package GirisDersi;
-
-public class Baslangic {
-    public static void main(String[] args) {
-        int not=46;
-        System.out.println("Hello, World!");
-        System.out.println(not>45 ? "Gecti" : "Kaldi");
-    }
-}
--- Java-101/src/KosulluIfadelerveKodBloklari/Siralama.java
+++
@@ -1,4 +0,0 @@
-package KosulluIfadelerveKodBloklari;
-
-public class Siralama {
-}
