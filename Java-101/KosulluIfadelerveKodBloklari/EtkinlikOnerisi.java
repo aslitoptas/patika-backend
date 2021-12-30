f36f61039838bd0b26781e0de6c6917b9a037081
Aslı Temel <asltpts@gmail.com>
2021-12-30 23:45:28 +0300

    First commit

--- Java-101/KosulluIfadelerveKodBloklari/EtkinlikOnerisi.java
+++ Java-101/KosulluIfadelerveKodBloklari/EtkinlikOnerisi.java
@@ -1,4 +1,22 @@
 package KosulluIfadelerveKodBloklari;
+import java.util.Scanner;
 
 public class EtkinlikOnerisi {
+    public static void main(String[] args) {
+        int sicaklik;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Hava Sıcaklığını Giriniz: ");
+        sicaklik = input.nextInt();
+
+        if(sicaklik > 5 && sicaklik <= 15){
+            System.out.print("Sinemaya gidebilirsiniz.");
+        }else if(sicaklik > 15 && sicaklik <= 25){
+            System.out.print("Piknik yapabilirsiniz.");
+        }else if(sicaklik > 25){
+            System.out.print("Yüzmeye gidebilirsiniz.");
+        }else{
+            System.out.print("Kayak yapabilirsiniz.");
+        }
+    }
 }
