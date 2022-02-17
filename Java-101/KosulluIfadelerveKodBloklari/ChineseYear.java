6417227a02d82821f1dc6ea5bd6d9ab6ee1927c2
Aslı Temel <asltpts@gmail.com>
2022-02-17 23:47:31 +0300

    First commit

--- Java-101/KosulluIfadelerveKodBloklari/ChineseYear.java
+++ Java-101/KosulluIfadelerveKodBloklari/ChineseYear.java
@@ -1,4 +1,28 @@
 package KosulluIfadelerveKodBloklari;
+import java.util.Scanner;
 
 public class ChineseYear {
+    public static void main(String[] args) {
+        int year, cYear;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınız: ");
+        year = input.nextInt();
+        cYear = year%12;
+        System.out.print("Çin Yılı: ");
+        switch (cYear) {
+            case 1 -> System.out.print("Horoz");
+            case 2 -> System.out.print("Köpek");
+            case 3 -> System.out.print("Domuz");
+            case 4 -> System.out.print("Fare");
+            case 5 -> System.out.print("Öküz");
+            case 6 -> System.out.print("Kaplan");
+            case 7 -> System.out.print("Tavşan");
+            case 8 -> System.out.print("Ejderha");
+            case 9 -> System.out.print("Yılan");
+            case 10 -> System.out.print("At");
+            case 11 -> System.out.print("Koyun");
+            default -> System.out.print("Maymun");
+        }
+    }
 }
