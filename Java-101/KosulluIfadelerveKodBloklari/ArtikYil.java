c8c3bd42e30a9c322aac40efd550366ddd7a57e9
Aslı Temel <asltpts@gmail.com>
2022-02-17 23:55:14 +0300

    First commit

--- Java-101/KosulluIfadelerveKodBloklari/ArtikYil.java
+++ Java-101/KosulluIfadelerveKodBloklari/ArtikYil.java
@@ -1,4 +1,22 @@
 package KosulluIfadelerveKodBloklari;
+import java.util.Scanner;
 
 public class ArtikYil {
+    public static void main(String[] args) {
+        int year;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Yılı Giriniz: ");
+        year = input.nextInt();
+
+        if(year%400==0){
+            System.out.print(year + " bir artık yıldır!");
+        }else{
+            if(year%4==0 && year%100!=0){
+                System.out.print(year + " bir artık yıldır!");
+            }else{
+                System.out.print(year + " artık yıl değildir!");
+            }
+        }
+    }
 }
